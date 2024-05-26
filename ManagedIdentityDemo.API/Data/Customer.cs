db4938e417bfab539dce0d25fcfdab42387e45ec
Shahab <shahabganji@gmail.com>
2024-05-26 11:27:09 +0200

    chore: removed warnings

--- ManagedIdentityDemo.API/Data/Customer.cs
+++ ManagedIdentityDemo.API/Data/Customer.cs
@@ -3,6 +3,6 @@ namespace ManagedIdentityDemo.API.Data;
 public sealed class Customer
 {
     public Guid Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
 }
